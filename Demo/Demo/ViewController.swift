9f787e923fea7a04fbe86cccc2332a19c40172c1
刘明杰 <balu@liumingjiedeMacBook-Pro.local>
2017-07-26 16:01:15 +0800

    2.1.5

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - property
+    @IBOutlet weak var tableView: UITableView!{
+        didSet{
+            tableView.delegate = self
+            tableView.dataSource = self
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        }
+    }
+    
+    fileprivate var dataSource:[String] = ["showTextAndHiddenView",
+                                           "showSuccessAndHiddenView",
+                                           "showErrorAndHiddenView",
+                                           "showView",
+                                           "hiddenAll"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,18 +34,38 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func showTexAndHidden(_ sender: UIButton) {
-        MGProgressHUD.showTextAndHiddenView("测试啊")
-    }
-
-    @IBAction func success(_ sender: AnyObject) {
-        MGProgressHUD.showSuccessAndHiddenView("成功")
-    }
     
-    @IBAction func failed(_ sender: UIButton) {
-        MGProgressHUD.showErrorAndHiddenView("失败")
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        MGProgressHUD.hiddenAllhubToView(view, animated: true)
     }
     
 }
 
+extension ViewController:UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        cell.textLabel?.text = dataSource[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            MGProgressHUD.showTextAndHiddenView("测试啊")
+        case 1:
+            MGProgressHUD.showSuccessAndHiddenView("成功")
+        case 2:
+            MGProgressHUD.showErrorAndHiddenView("失败")
+        case 3:
+            MGProgressHUD.showView(view, icon: "空数据", message: "页面载入失败咯", detailText: "")
+        case 4:
+            MGProgressHUD.hiddenAllhubToView(view, animated: true)
+        default:
+            return
+        }
+    }
+}
